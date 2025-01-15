85ea2667d892f0997fa5dba463f11d068a0473b6
Mohamed <mohamed@deepscenario.com>
2025-01-15 16:01:38 +0100

    Correct key format to prevent parsing errors in CI.yml

--- restart_docker.sh
+++ restart_docker.sh
@@ -5,6 +5,7 @@ then
   echo "Using the metadata stored in the virtual environment directory"
   export $(jq -r 'to_entries | .[] | "\(.key)=\(.value)"' .venv/database_metadata.json)
   docker compose down --volumes
+  docker compose rm -f
   docker compose up -d --force-recreate --build
   unset $(jq -r 'keys[]' .venv/database_metadata.json)
 else
@@ -15,5 +16,6 @@ else
   export database_server_pwd="server_pwd"
   export host="localhost"
   export db_port=5800
+  docker compose rm -f
   docker compose up --force-recreate --build
 fi
