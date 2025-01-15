c2f9a7a6316d3795154757659e339243f5154003
Mohamed <mohamed@deepscenario.com>
2025-01-15 15:46:50 +0100

    Correct key format to prevent parsing errors in CI.yml

--- tests/test_annotation.py
+++ tests/test_annotation.py
@@ -1,5 +1,12 @@
+import pytest
+
 from src.utils import Annotation
 
 
 def test_pyannote_key_exists():
     Annotation()
+
+
+@pytest.mark.parametrize("n, expected", [(1, []), (2, []), (3, [])])
+def test_annotate(n: int, expected: list[tuple[str, float, float]]):
+    pass
